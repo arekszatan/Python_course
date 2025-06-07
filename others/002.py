063286e62aed94f48143806532863a93d226aa00
asza <arek.szatan@interia.pl>
2025-06-07 00:30:41 -0700

    01_variables_declaration commit

---
+++ others/002.py
@@ -0,0 +1,20 @@
+productName = "Smartphone"
+productPrice = 1200
+
+print(productName + " after discount: ")
+print(productPrice * 0.75)
+
+# bad name, hard to remember and know what is inside variable
+x4rtZ = "Hello World"
+
+# good name for variable, inside and variable name is good
+welcomeMessage = "Hello World"
+
+topSpeed = 200
+topSPEED = 330
+
+# 200
+print(topSpeed)
+
+#330
+print(topSPEED)
