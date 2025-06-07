1ffc5c8815c6874990b1d8c89750b9042e92e4cb
asza <arek.szatan@interia.pl>
2025-06-07 03:09:09 -0700

    04_type_boolen commit

---
+++ basics/01_data_types/04_type_boolean.py
@@ -0,0 +1,15 @@
+
+t = True
+f = False
+
+print(type(t)) # <class 'bool'>
+
+print(20 > 6) # True
+print(4 < 2) # False
+
+number = 10
+
+if number > 5:
+    print("number is greater than 5")
+else:
+    print("number is less then 5")
