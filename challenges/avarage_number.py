8574909cfd054c747f8de2e6e169b6483f468247
asza <arek.szatan@interia.pl>
2025-06-07 01:00:30 -0700

    average_number commit

---
+++ challenges/avarage_number.py
@@ -0,0 +1,14 @@
+# 1. Oblicz średnią z następujących liczb 1, 2, 3 oraz 4
+# Przypisz obliczoną wartość do zmiennej avarageNum
+# Przy obliczeniach pamiętaj aby skorzystać z nawiasów
+# dla prawidłowiej kolejności działań
+# 2. Pokaż w konsoli średnią z liczb
+# 3. Pokaż rówież typ tej średniaj z funkcją type
+
+numberOne = 1
+numberTwo = 2
+numberThree = 3
+numberFour = 4
+averageNum = (numberOne + numberTwo + numberThree + numberFour) / 4
+print("Średnia z liczb:",averageNum)
+print(type(averageNum))
