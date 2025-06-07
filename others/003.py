441df8cd2eccd326c64213766c95775aa7a7f99a
asza <arek.szatan@interia.pl>
2025-06-07 01:21:35 -0700

    003 file commit

---
+++ others/003.py
@@ -0,0 +1,17 @@
+text = "Hello!"
+print(text) # Hello!
+print(type(text)) # <class 'str'>
+
+print(text[0])
+print(text[1:4])
+print(text * 3)
+print(text + " Everyone!")
+
+print("Szkolenie Python'a")
+
+textLong = """
+Pewien tekst 
+Kontynuacja tekstu w nowej lini 
+korzystanie z znaku nowej lini \n nowy tekst
+"""
+print(textLong)
