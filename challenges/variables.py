f902899248cdd0eac073f2f0f2d1d6627d0f1aa0
asza <arek.szatan@interia.pl>
2025-06-07 00:41:57 -0700

    variables file commit

---
+++ challenges/variables.py
@@ -0,0 +1,20 @@
+# zapisz zmienne z identyfikatorami ktore najlepiej
+# opisują wiadomość email np. tytół, treść, nadawca itd.
+# Zapisz przy okazji przykładowe wartości w tych zmiennnych
+# Wyświetl te zmienne w konsoli za pomocą funkcji print()
+
+title = "Pierwszy email"
+content = "Hello World"
+sender = "Asia Kowalska"
+receiver = "Ola"
+emailDate = "20xx.10.09"
+numAttachments = 1
+attachmentName = "file.doc"
+
+print("Tytuł maila: " + title)
+print("Treść emaila: " + content)
+print("Nadawca: " + sender)
+print("Odbiorca: " + receiver)
+print("Data maila: " + emailDate)
+print("Ilość załączników:", numAttachments)
+print("Załącznik nr. 1: " + attachmentName)
