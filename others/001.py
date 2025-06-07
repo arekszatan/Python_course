d9c4b178e73658960f252b605fe0a980dd1e6b3a
asza <arek.szatan@interia.pl>
2025-06-07 00:46:00 -0700

    file 001 actualization

--- others/001.py
+++ others/001.py
@@ -1,1 +1,16 @@
-print("Hello World")
+print("Hello World") # komentarz
+# komentarz
+# print("Hello again")
+
+# 1
+# 2
+# 3
+
+"""
+dowolny test w 
+wielu
+liniach
+!
+"""
+
+print("Hello World!")
