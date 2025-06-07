0c810ea908f57868d78807ff0a1d1906b3e17c06
asza <arek.szatan@interia.pl>
2025-06-07 07:26:48 -0700

    07_type_dict file commit

---
+++ basics/01_data_types/07_type_dict.py
@@ -0,0 +1,22 @@
+
+contacts = {
+    "Ola" : "ola@example.com",
+    "Daniel" : 30,
+    "Ania" : "ania@example.com"
+}
+
+contacts["Rafał"] = "rafał@example.com"
+
+print(contacts["Ola"])
+print(contacts["Daniel"])
+print(type(contacts))
+print(len(contacts))
+
+print(contacts.keys())
+print(contacts.values())
+
+for key in contacts:
+    print(key + " " + str(contacts[key]))
+
+for key, value in contacts.items():
+    print(f'key: {key}, value: {value}')
