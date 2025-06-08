731b3bbf4a971f7665b76b353fd5b15a32db622b
asza <arek.szatan@interia.pl>
2025-06-08 03:38:02 -0700

    01_variables_declarations_2, 02_type_int and set_test commit

---
+++ basics/01_data_types/02_type_int.py
@@ -0,0 +1,22 @@
+
+# declaration of variables
+number1 = 10
+number2 = 40
+number3 = 50
+
+# addition
+result_of_addition = number1 + number2 + number3
+print(f'Result of summing number1 , number2 and number3 is {result_of_addition}')
+
+# substraction
+result_of_substraction = number3 - number1
+print(f'Result of substraction number3 and number1 is {result_of_substraction}')
+
+# multiplication
+result_of_multiplication = number1 * number2 * number3
+print(f'Result of multiplication number1, number2 and number3 is {result_of_multiplication}')
+
+# division
+result_of_division = number3 / number2
+print(f'Result of division number3 and number2 is {result_of_division}')
+
---
+++ basics/01_data_types_2/01_variables_declarations_2.py
@@ -0,0 +1,25 @@
+
+number1 = 100
+number2 = 200
+result = number1 + number2
+print(f'Result of adding number1 to number2 is {result}')
+
+new_number = (result * 3) / 4
+print(f'New number is type {type(new_number)} and value of new_number is {new_number}')
+
+text1 = "First text for sentence"
+text2 = "Second text for the same sentence"
+text3 = "Third text waiting with second sentence"
+text4 = "Fourth text nothing interesting"
+text5 = "Fifth text something interesting"
+
+text_sum = text1 + text2 + text3 + text4 + text5
+print(text_sum)
+
+# Sum two text like boss
+print(f'First text is {text1}, second text is {text2} and sum of text1 and text2 is {text1}{text2}')
+
+number1 = 342
+number2 = 3424
+result_of_addition = number1 + number2
+print(f'Result of summing number1 and number2 is {result_of_addition}')
