a89cde8ced2153a333025665acbb7f4fbf17cd7f
asza <arek.szatan@interia.pl>
2025-06-08 05:38:07 -0700

    12_type_none commit

---
+++ basics/01_data_types/12_type_none.py
@@ -0,0 +1,9 @@
+
+current_task_number = 10
+print(f'Value of current_task_number is {current_task_number}')
+
+current_task_number = None
+print(f'Value of current_task_number is {current_task_number}')
+
+current_task_number = 12
+print(f'Value of current_task_number is {current_task_number}')
