cec03f10a4ef9c0eb8671098a89dec645e7c0fa9
asza <arek.szatan@interia.pl>
2025-06-08 04:04:51 -0700

    09_type_converstion_int_float_str file commit

---
+++ basics/01_data_types/09_type_converstion_int_float_str.py
@@ -0,0 +1,23 @@
+
+float_num = 23.42323
+int_num = int(float_num)
+print(f'Type of int_num is {type(int_num)}')
+print(int_num)
+
+# print(int("  553425   fds")) # ValueError
+print(int("  553425   "))
+print(int(99))
+
+intNum = 65
+float1 = float(intNum)
+print(f'Type of float1 is {type(float1)}')
+print(float1)
+
+string = "123.34234"
+float2 = float(string)
+print(f'Type of flat2 is {type(float2)}')
+print(float2)
+print(float(89.342))
+
+print("Value float1: " + str(float1) + "\n" + str(4324.342))
+print("Wartość float1", float1, " ", float2)
