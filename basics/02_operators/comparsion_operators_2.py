ec0b23e8ff7244ce04e1e0df0dab5362b6ad5d23
asza <arek.szatan@interia.pl>
2025-06-09 04:36:24 -0700

    comparison operators file commmit

---
+++ basics/02_operators/comparsion_operators_2.py
@@ -0,0 +1,36 @@
+
+# Operatory - znak równości
+print("--------------------------")
+print("operator ==")
+print(f'7 == 8 : {7 == 8}') # False
+print(f'10 == 10 : {10 == 10}') # True
+
+print("--------------------------")
+print(f'operator !=')
+print(f'3 != 41 : {3 != 41}') # True
+print(f'5 != 5 : {5 != 5}') # False
+
+print("--------------------------")
+print(f'operator <')
+print(f'3 < 4 : {3 < 4}') # True
+print(f'6 < 6 : {6 < 6}') # False
+print(f'4 < 3 : {4 < 3}') # False
+
+print("--------------------------")
+print(f'operator >')
+print(f'3 > 4 : {3 > 4}') # False
+print(f'4 > 4 : {4 > 4}') # False
+print(f'4 > 3 : {4 > 3}') # True
+
+print("--------------------------")
+print(f'operator >=')
+print(f'3 >= 4 : {3 >= 4}') # False
+print(f'6 >= 6 : {6 >= 6}') # True
+print(f'4 >= 3 : {4 >= 3}') # True
+
+print("--------------------------")
+print(f'operator <=')
+print(f'3 <= 4 : {3 <= 4}') # True
+print(f'6 <= 6 : {6 <= 6}') # True
+print(f'4 <= 3 : {4 <= 3}') # False
+
