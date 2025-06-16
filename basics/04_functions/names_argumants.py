d226920c168caef837ca52f650244e6f489cd678
asza <arek.szatan@interia.pl>
2025-06-16 11:00:31 -0700

    names arguments commit

---
+++ basics/04_functions/names_argumants.py
@@ -0,0 +1,9 @@
+
+def print_car(brand, name = "concept", year = 1960, color = "black"):
+    print(brand, name, year, color)
+
+print_car(name = "T", brand = "Mustang")
+print_car(name = "T", brand = "Mustang", year = 1960)
+
+
+
