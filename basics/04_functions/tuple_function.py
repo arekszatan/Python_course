41d7ba230fcccf985c4037274d5b8afa1b080673
asza <arek.szatan@interia.pl>
2025-06-23 05:40:07 -0700

    tuple function commit

---
+++ basics/04_functions/tuple_function.py
@@ -0,0 +1,14 @@
+
+tuple1 = (1, 2, 3, 4) + (5, 6) + (7,) + tuple([8, 9])
+print(type(tuple1))
+print(tuple1)
+
+print((1, 2) * 4)
+
+print(9 in tuple1)
+print(0 in tuple1)
+print(tuple1[1])
+print(len(tuple1))
+
+print(min(tuple1))
+print(max(tuple1))
