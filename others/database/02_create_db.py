691043ab6a599e84e285c8c39e0f81ecdef37039
asza <arek.szatan@interia.pl>
2025-06-29 03:06:06 -0700

    02_create_db.py commit

---
+++ others/database/02_create_db.py
@@ -0,0 +1,41 @@
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+# Dane do połączenia z bazą danych PostgreSQL
+host = "localhost"
+user = "postgres"
+password = "admin"
+database = "postgres" # Łączymy się z bazą domyślną, aby stworzyć nową bazę danych
+
+try:
+    # Nawiązywanie połączenia z bazą danych
+    connection = psycopg2.connect(
+        host=host,
+        user=user,
+        password=password,
+        dbname=database
+    )
+    # Ustawiane poziomu izolacji na AUTOCOMMIT
+    # Pozwala to na natychmiastowe wykonanie poleceń SQL bez konieczności wywołania
+    # commit()
+    # Jest to potrzebne, ponieważ niektóre polecenia, takie jak tworzenie baz danych,
+    # muszą być wykonywane poza transakcją.
+    connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+
+    # Utworzenie kursora
+    cursor = connection.cursor()
+
+    # Tworzenie nowej bazy danych
+    cursor.execute("CREATE DATABASE py_test2")
+    print("Baza danych py_test2 została pomyślnie utworzona.")
+
+
+except (Exception, psycopg2.DatabaseError) as error:
+    print("Błąd podczas połączenia z bazą danych PostgreSQL", error)
+finally:
+    # Zawsze zamykaj połączenie i kursor, aby zwolnić zasoby
+    if connection:
+        print("Połączenie z bazą działa.")
+        cursor.close()
+        connection.close()
+        print("Połączenie z bazą zostało zamknięte.")
