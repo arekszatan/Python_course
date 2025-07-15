55150f7171f50e2535b88009a4aee6d1497bfb65
arek <arek.szatan@interia.pl>
2025-07-15 16:20:04 +0200

    18_limit_offset.py

---
+++ others/database/18_limit_offset.py
@@ -0,0 +1,33 @@
+import psycopg2
+
+host = "127.0.0.1"
+user = "postgres"
+password = "test123"
+database = "py_test"
+
+connection = None
+cursor = None
+try:
+    connection = psycopg2.connect(
+        host=host,
+        user=user,
+        password=password,
+        database=database
+    )
+
+    cursor = connection.cursor()
+
+    print("3 rekordy z tabeli 'employees', zaczynające od trzeciego rekordu:")
+    cursor.execute("SELECT * FROM employees LIMIT 3 OFFSET 3")
+    for row in cursor.fetchall():
+        print(row)
+
+
+except (Exception, psycopg2.DatabaseError) as error:
+    print("Błąd podczas połączenia z bazą danych PostgreSQL", error)
+finally:
+    if connection:
+        print("Połączenie z bazą działa.")
+        cursor.close()
+        connection.close()
+        print("Połączenie z bazą zostało zamknięte.")
