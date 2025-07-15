675af7e8b64e733ae48111d86c9733c26aebaa86
arek <arek.szatan@interia.pl>
2025-07-15 16:26:10 +0200

    20_null.py commit

---
+++ others/database/20_null.py
@@ -0,0 +1,32 @@
+import psycopg2
+
+host = "127.0.0.1"
+user = "postgres"
+password = "test123"
+database = "py_test"
+
+connection = None
+cursor = None
+try:
+    connection = psycopg2.connect(
+        host=host,
+        user=user,
+        password=password,
+        database=database
+    )
+
+    cursor = connection.cursor()
+
+    print("Pracownicy byz określonego adresu email:")
+    cursor.execute("SELECT * FROM employees WHERE email IS NULL")
+    for row in cursor.fetchall():
+        print(row)
+
+except (Exception, psycopg2.DatabaseError) as error:
+    print("Błąd podczas połączenia z bazą danych PostgreSQL", error)
+finally:
+    if connection:
+        print("Połączenie z bazą działa.")
+        cursor.close()
+        connection.close()
+        print("Połączenie z bazą zostało zamknięte.")
